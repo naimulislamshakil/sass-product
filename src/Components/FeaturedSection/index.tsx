42da0d720724258eec5b57b1998799d73095f2b2
Naimul Islam <ms0413354@gmail.com>
2024-04-07 09:21:16 +0600

    add title and description in featured section

---
+++ src/Components/FeaturedSection/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const FeaturedSection = () => {
+	return (
+		<div className="mx-auto mt-32 mb-32 sm:mt-56 max-w-5xl">
+			<div className="md-12 px-6 lg:px-8">
+				<div className="mx-auto max-w-2xl sm:text-center">
+					<h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+						Start chatting in minutes
+					</h2>
+					<p className="mt-4 text-lg text-gray-600">
+						Chatting to your PDF files has never been easire than with Quill
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default FeaturedSection;
--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import ValueProposition from '../ValueProposition';
+import FeaturedSection from '../FeaturedSection';
 
 const Home = () => {
 	return (
 		<div>
 			<Banner />
 			<ValueProposition />
+			<FeaturedSection />
 		</div>
 	);
 };
